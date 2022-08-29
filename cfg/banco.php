84fd9857651f42b38e4237e573bd684c6f31ec23
jhonathandelgado16 <102108853+jhonathandelgado16@users.noreply.github.com>
2022-08-29 11:33:19 -0300

    add cfg

---
+++ cfg/banco.php
@@ -0,0 +1,10 @@
+<?php
+
+$banco = [
+    'driver' => 'mysql',
+    'servidor' => 'localhost',
+    'porta' => '3306',
+    'banco' => 'caderneta_pelotao',
+    'usuario' => 'root',
+    'senha' => '160315',
+];
